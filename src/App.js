ade2ae39e0a05ac0489161217514e58d8ca32fbb
eliasoooo <elias-ghanem@outlook.com>
2025-01-27 01:48:49 +0100

    fixing errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
+  const token = localStorage.getItem("access_token");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {!token ? (
+        <>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </>
+      ) : (
+        // Remove the Dashboard route or replace it with any other component if needed
+        <Route path="/" element={<div>Welcome to the app!</div>} />
+      )}
+    </Routes>
   );
 }
 
